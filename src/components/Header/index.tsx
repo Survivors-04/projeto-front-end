71bc14679a761e5dc167b4d3970f905c9dbbcde1
Edmar Cardoso <ed2019junior@gmail.com>
2022-08-30 12:04:19 -0300

    feat: inital-header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,26 @@
+import { Container } from "./style";
+import iconUserHeader from "../../assets/imgs/iconUserHeader.svg";
+import logoHeader from "../../assets/imgs/logoHeader.svg";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <div>
+        <img src={logoHeader} alt="" />
+        <nav>
+          <a href="">Início</a>
+          <a href="">Recompensa</a>
+          <a href="">Mercado</a>
+          <a href="">Ajuda</a>
+          <a href="">Sobre Nós</a>
+        </nav>
+        <span>100g</span>
+        <button>
+          <img src={iconUserHeader} alt="" />
+        </button>
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
---
+++ src/components/Header/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background-color: var(--color-red-focus);
+  width: 100%;
+  height: 80px;
+`;
