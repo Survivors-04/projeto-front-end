70a2144d07677ab21fb161eb0f3945771a6eb11e
Enrique <enrique.barbosasilva@gmail.com>
2022-08-31 19:27:21 -0300

    refactor: button-changed

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,28 +1,36 @@
-
 import StyledContainer from "../../components/Container/styles";
 import { ContainerLogin } from "./styles";
 import { Form } from "../../components/Form";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
 
- const Login = () => {
+const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <StyledContainer>
+      <ContainerLogin>
+        <main className="main">
+          <h2 className="title">Login</h2>
 
-    <ContainerLogin>
-       
-      <main className="main">
-       <h2 className="title">Login</h2>
-      
-       <Form>
-       <button type="submit">Entrar</button>
-       </Form>
+          <Form>
+            <Button width={25}>Entrar</Button>
+          </Form>
 
-        <div className="register">
-          <p>Ainda não possui uma conta?</p>
-          <img src={"https://www.pkparaiso.com/imagenes/xy/sprites/animados/charmander.gif"} alt="charmander" />
-          <button>Cadastrar</button>
-        </div>
-      </main>
-    </ContainerLogin>
+          <div className="register">
+            <p>Ainda não possui uma conta?</p>
+            <img
+              src={
+                "https://www.pkparaiso.com/imagenes/xy/sprites/animados/charmander.gif"
+              }
+              alt="charmander"
+            />
+            <Button width={25} onClick={() => navigate("/register")}>
+              Cadastrar
+            </Button>
+          </div>
+        </main>
+      </ContainerLogin>
     </StyledContainer>
   );
 };
