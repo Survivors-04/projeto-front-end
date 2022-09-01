0906b15a529be2d8f3c67545b6da0f02b9bf542d
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-09-01 17:48:10 -0300

    refactor: button-props

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 
 interface iStyledButton {
-  width: number;
+  width?: number;
+  hover?: string;
+  textColor?: string;
+  backgroundColor?: string;
 }
 
 const StyledButton = styled.button<iStyledButton>`
-  width: ${({ width }) => width}%;
-  background: var(--color-red);
+  width: ${({ width }) => (width ? width : 100)}%;
+
+  color: ${({ textColor }) => (textColor ? textColor : "var(--color-gray-4)")};
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor ? backgroundColor : "var(--color-red)"};
 
   margin: 5px 0;
   padding: 10px 0;
@@ -14,12 +20,13 @@ const StyledButton = styled.button<iStyledButton>`
   font-weight: 900;
   font-size: 18px;
 
-  border: 3px solid black;
+  border: 3px solid ${({ textColor }) => (textColor ? textColor : "black")};;
   border-radius: 8px;
   box-shadow: 1.6px 1.6px;
 
   :hover {
-    background-color: var(--color-red-focus);
+    background-color: ${({ hover }) =>
+      hover ? hover : "var(--color-red-focus)"};
     transform: translate(-1px, -1px);
     box-shadow: 2.4px 2.4px;
   }
