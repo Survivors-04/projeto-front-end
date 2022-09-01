617c6da3d7f4a06c3af3d31e202f2b67ebc27b5a
Enrique <enrique.barbosasilva@gmail.com>
2022-09-01 16:08:39 -0300

    style: modified-styling

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 const StyledContainer = styled.div`
   margin: 0 auto;
-  
+
   width: 95%;
 
   display: flex;
   align-items: center;
   justify-content: center;
-  
+
   @media screen and (min-width: 768px) {
     width: 75%;
   }
