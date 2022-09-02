16d370809afe5af432cc5bf9b157e04937b92248
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-09-02 13:34:41 -0300

    refactor: header-mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,11 @@
-import { Nav, StyledContainer, StyledDiv, StyledNavButton } from "./style";
+import {
+  Nav,
+  StyledContainer,
+  StyledDiv,
+  StyledHeaderContainer,
+  StyledHeaderMobile,
+  StyledNavButton,
+} from "./style";
 import logoHeader from "../../assets/imgs/HeaderSvg/logoHeader.svg";
 import iconUserHeader from "../../assets/imgs/HeaderSvg/iconUserHeader.svg";
 import menuLogo from "../../assets/imgs/HeaderSvg/menu.svg";
@@ -18,63 +25,68 @@ const Header = () => {
   return (
     <>
       <StyledContainer>
-        <StyledDiv>
-          <img src={logoHeader} alt="ImageGroup4" />
+        <StyledHeaderContainer>
+          <StyledDiv>
+            <img src={logoHeader} alt="ImageGroup4" />
+          </StyledDiv>
 
-          <button onClick={() => setIsModalHeader(true)}>
-            <img src={menuLogo} alt="" />
-          </button>
-          {isModalHeader && <ModalHeader />}
-        </StyledDiv>
+          <StyledHeaderMobile>
+            <button onClick={() => setIsModalHeader(true)}>
+              <img src={menuLogo} alt="" />
+            </button>
+          </StyledHeaderMobile>
 
-        <Nav>
-          <ul>
-            <li>
-              <Link to={"/"}>Início</Link>
-            </li>
-            <li>
-              <StyledNavButton onClick={() => setIsModalDice(true)}>Recompensa</StyledNavButton>
-            </li>
-            <li>
-              <Link to={"/marketplace"}>Mercado</Link>
-            </li>
-            <li>
-              <Link to={"/help"}>Ajuda</Link>
-            </li>
-            <li>
-              <Link to={"/aboutUs"}>Sobre nós</Link>
-            </li>
-            <>
-              {isLogged ? (
-                <>
+          <Nav>
+            <ul>
+              <li>
+                <Link to={"/"}>Início</Link>
+              </li>
+              <li>
+                <StyledNavButton onClick={() => setIsModalDice(true)}>
+                  Recompensa
+                </StyledNavButton>
+              </li>
+              <li>
+                <Link to={"/marketplace"}>Mercado</Link>
+              </li>
+              <li>
+                <Link to={"/help"}>Ajuda</Link>
+              </li>
+              <li>
+                <Link to={"/aboutUs"}>Sobre nós</Link>
+              </li>
+              <>
+                {isLogged ? (
+                  <>
+                    <li>
+                      <span>100g</span>
+                    </li>
+                    <li>
+                      <StyledNavButton onClick={() => setIsLogged(false)}>
+                        <img src={iconUserHeader} alt="" />
+                      </StyledNavButton>
+                    </li>
+                  </>
+                ) : (
                   <li>
-                    <span>100g</span>
+                    <Button
+                      width={100}
+                      onClick={() => navigate("/login", { replace: true })}
+                      // onClick={() => setIsLogged(true)}
+                      textColor={"var(--color-blue)"}
+                      backgroundColor={"var(--color-yellow)"}
+                      hover={"var(--color-yellow-focus)"}
+                    >
+                      Login
+                    </Button>
                   </li>
-                  <li>
-                    <button onClick={() => setIsLogged(false)}>
-                      <img src={iconUserHeader} alt="" />
-                    </button>
-                  </li>
-                </>
-              ) : (
-                <li>
-                  <Button
-                    width={100}
-                    onClick={() => navigate("/login", { replace: true })}
-                    // onClick={() => setIsLogged(true)}
-                    textColor={"var(--color-blue)"}
-                    backgroundColor={"var(--color-yellow)"}
-                    hover={"var(--color-yellow-focus)"}
-                  >
-                    Login
-                  </Button>
-                  {/* <Link to={"/login"}>Login</Link> */}
-                </li>
-              )}
-            </>
-          </ul>
-        </Nav>
+                )}
+              </>
+            </ul>
+          </Nav>
+        </StyledHeaderContainer>
       </StyledContainer>
+      {isModalHeader && <ModalHeader />}
       {isModalDice && <DiceRoll />}
     </>
   );
--- src/components/Modal/ModalHeader/style.ts
+++ src/components/Modal/ModalHeader/style.ts
@@ -1,47 +1,35 @@
 import styled from "styled-components";
 
-export const StyledDiv = styled.div`
-  width: 100%;
-  display: flex;
-  top: 0px;
-  height: 100vh;
+export const StyledSideHeader = styled.ul`
+  height: 100%; 
+  width: 60%;
+  max-width: 200px;
   position: fixed;
+  z-index: 1; 
+  top: 0; 
+  right: 0;
+  background-color: var(--color-red);
+  overflow-x: hidden;
+  padding-top: 60px; 
+  transition: 0.5s;
 
-  @media screen and (min-width: 700px) {
-    background: none;
+  li {
+    color: var(--color-gray-4);
+    padding: 15px;
+
+    text-decoration: none;
+    font-size: 20px;
+
+    display: block;
+
+    transition: 0.3s;
   }
-  .div-transition {
-    ul {
-      width: 100%;
-      background-color: var(--color-red);
-      border: 1px solid #ccc;
-      border-radius: 0.3em;
-      box-shadow: 3px 3px 8px rgba(0, 0, 0, 0.2);
-      position: absolute;
-      margin-top: 80px;
-      transition: opacity 0.3s ease;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-      gap: 15px;
-      height: 70%;
 
-      > li {
-        display: flex;
-        gap: 10px;
-        padding: 0.2em 0.4em;
-        color: var(--color-gray-4);
+  li > svg {
+    margin-right: 5px;
+  }
 
-        a {
-          font-size: 16px;
-        }
-      }
-    }
-
-    @media screen and (min-width: 700px) {
-      ul {
-        display: none;
-      }
-    }
+  li > span {
+    margin-left: 5px;
   }
 `;
