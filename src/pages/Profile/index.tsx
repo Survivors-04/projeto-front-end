d0ce46101e11385934fd737cf3e903e96884775c
Enrique <enrique.barbosasilva@gmail.com>
2022-09-02 13:23:57 -0300

    feat: user-info-add

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -5,40 +5,47 @@ import imgBulba from "../../assets/imgs/Profile/bulbasaur2 1.svg";
 import imgAsh from "../../assets/imgs/Profile/ash 1.svg";
 import imgCharmander from "../../assets/imgs/Profile/Charmander.svg";
 import { StyledCharmImg, StyledDiv, StyledList, StyledSection } from "./styles";
+import api from "../../services/api";
+import { useEffect, useState } from "react";
+
+interface IData {
+  email: string;
+  password: string;
+}
+
+interface IPokemons {
+  Pokemon: string;
+  Rarity: string;
+  Number: number;
+  Type01: string;
+  Type02: string;
+  userId: number;
+  id: number;
+}
 
 const Profile = () => {
-  const colection = [
-    {
-      name: "Pikachu",
-      class: "Elétrico",
-      rarity: "Comum",
-    },
-    {
-      name: "Pikachu",
-      class: "Elétrico",
-      rarity: "Comum",
-    },
-    {
-      name: "Bulbasaur",
-      class: ["Grama", "Veneno"],
-      rarity: "Comum",
-    },
-    {
-      name: "Bulbasaur",
-      class: ["Àgua", "Pedra"],
-      rarity: "Comum",
-    },
-    {
-      name: "Pikachu",
-      class: "Elétrico",
-      rarity: "Comum",
-    },
-    {
-      name: "Pikachu",
-      class: "Elétrico",
-      rarity: "Comum",
-    },
-  ];
+  const [userName, setUserName] = useState("");
+  const [pokemons, setPokemons] = useState<IPokemons[]>([]);
+
+  const data = {
+    email: "teste5@gamil.com",
+    password: "123456",
+  };
+
+  useEffect(() => {
+    api.get("/Users/5/pokedexUser").then((response) => {
+      setPokemons(response.data);
+    });
+  }, [pokemons]);
+
+  const login = (data: IData) => {
+    api.post("/login", data).then((response) => {
+      window.localStorage.setItem("@TOKEN", response.data.accessToken);
+      setUserName(response.data.user.name);
+    });
+  };
+
+  login(data);
 
   return (
     <>
@@ -51,7 +58,7 @@ const Profile = () => {
             </figure>
 
             <ul>
-              <li>Ash</li>
+              <li>{userName}</li>
               <li>Coleção: 15</li>
               <li>Raros: 2</li>
               <li>
@@ -77,19 +84,19 @@ const Profile = () => {
                 onChange={() => {}}
               />
             </form>
-            {colection.length >= 1 &&
-              colection.map((pokemon, index) => (
+            {pokemons.length >= 1 &&
+              pokemons.map((pokemon, index) => (
                 <li key={index}>
                   <figure>
                     <img
-                      src={`https://www.pkparaiso.com/imagenes/xy/sprites/animados/${pokemon.name.toLowerCase()}.gif`}
+                      src={`https://www.pkparaiso.com/imagenes/xy/sprites/animados/${pokemon.Pokemon.toLowerCase()}.gif`}
                       alt=""
                     />
                   </figure>
 
-                  <h3>{pokemon.name}</h3>
-                  <p>{pokemon.class}</p>
-                  <p>{pokemon.rarity}</p>
+                  <h3>{pokemon.Pokemon}</h3>
+                  <p>{pokemon.Type01}</p>
+                  <p>{pokemon.Rarity}</p>
                 </li>
               ))}
           </StyledList>
