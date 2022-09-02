257e6de0bf06bc585b1674ceef7f52e4bc2ca7d8
Enrique <enrique.barbosasilva@gmail.com>
2022-09-02 11:25:01 -0300

    feat: baseurl-api-created

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
-const api = () => {
-  return "oi";
-};
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: " https://projeto-front-end-json-server.herokuapp.com",
+});
 
 export default api;
