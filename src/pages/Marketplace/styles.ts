aba0b8d60de3aa3120411579bfa79b52cac34865
Paulo Gustavo <arknum@hotmail.com>
2022-09-02 10:20:56 -0300

    feat: marketplace-responsivity

--- src/pages/Marketplace/styles.ts
+++ src/pages/Marketplace/styles.ts
@@ -5,8 +5,22 @@ display: flex;
 flex-direction: row;
 margin-left: 10%;
 
-`
+@media only screen and (max-width: 779px) {
+    
+    margin: 0 auto;
+    width: 95%;
+    max-width: 875px;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    -webkit-box-align: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    
 
+}
+`
 
 export default StyledDivsMarket
 
@@ -15,10 +29,24 @@ display: flex;
 flex-wrap: wrap;
 flex-direction: row;
 width: 60vw;
-`
+@media only screen and (max-width: 779px) {
+    
+    display: flex;
+    flex-flow: row;
+    overflow: auto;
+    width: 80vw;
+    margin-left: 0;   
+
+}`
 
 export const StyledDivSearchCard = styled.div`
 display: flex;
 flex-direction: column;
 
+@media only screen and (max-width: 779px) {
+    
+    display: flex;
+    flex-direction: column;   
+
+}
 `
