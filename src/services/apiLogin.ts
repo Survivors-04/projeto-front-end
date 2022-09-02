a3ecca9f1df3119655d9dad42d6032faa2b4d605
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-09-02 14:32:10 -0300

    feat: apiRegister/services

--- src/services/apiLogin.ts
+++ src/services/apiLogin.ts
@@ -4,10 +4,9 @@ export interface IApiLogin{
 email?: string,
 password?:string,
  }
- const ApiLogin = (data:IApiLogin) => {
-  Api.post("/login",data)
+ const ApiLogin = async (data:IApiLogin) => {
+ await  Api.post("/login",data)
  .then((response)=>{
-  console.log(response)
   if(response.status === 200){
     window.localStorage.clear();
     window.localStorage.setItem("@TOKEN:", response.data.accessToken)
