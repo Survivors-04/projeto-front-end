63d50afccc795a7dfc3202e3592be0de159f923f
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-09-04 15:39:39 -0300

    fix: apiPost/apiLogin

--- src/services/apiLogin.ts
+++ src/services/apiLogin.ts
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import api from "./api";
 
 export interface IApiLogin {
@@ -6,14 +7,19 @@ export interface IApiLogin {
 }
 
 const ApiLogin = async (data: IApiLogin) => {
+  const navigate = useNavigate();
   try {
-    const response = await api.post("login", data);
+    const response = await api.post("/login", data);
 
     window.localStorage.clear();
     window.localStorage.setItem("@TOKEN", response.data.accessToken);
     window.localStorage.setItem("@USERID", response.data.user.id);
+
+    
   } catch (err) {
     console.log(err);
+  }finally{
+    navigate( "/", {replace:true} )
   }
 };
 
