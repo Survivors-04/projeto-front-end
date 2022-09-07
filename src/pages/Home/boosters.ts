d1512cac58be3640eabdba999b2daf57d79acb5c
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-09-07 20:03:28 -0300

    refactor: boosters-images

--- src/pages/Home/boosters.ts
+++ src/pages/Home/boosters.ts
@@ -1,4 +1,8 @@
 import booster_01 from "../../assets/imgs/boosters/booster_01.png";
+import booster_02 from "../../assets/imgs/boosters/booster_02.png";
+import booster_03 from "../../assets/imgs/boosters/booster_03.png";
+import booster_04 from "../../assets/imgs/boosters/booster_04.png";
+
 
 interface iBoosters {
   imgUrl: string;
@@ -13,17 +17,17 @@ const boosters: iBoosters[] = [
     price: 100,
   },
   {
-    imgUrl: booster_01,
+    imgUrl: booster_02,
     title: "Pacote 02",
     price: 200,
   },
   {
-    imgUrl: booster_01,
+    imgUrl: booster_03,
     title: "Pacote 03",
     price: 300,
   },
   {
-    imgUrl: booster_01,
+    imgUrl: booster_04,
     title: "Próxima geração"
   },
 ];
