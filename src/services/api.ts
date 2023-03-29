98da0c372bc11d20497d3017d6ea2c9aa6e66068
Edmar Cardoso <ed2019junior@gmail.com>
2023-03-29 16:06:59 -0300

    refactor: registerUserAndAdressApi

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " https://projeto-front-end-json-server.herokuapp.com",
+  baseURL: " http://127.0.0.1:8000/api/",
 });
 
 export default api;
