b373cc5ed9e0a4becbd0c365ae1cecc9e7db0daa
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-04-03 09:46:55 -0300

    refactor: Profile - pokemon use api implemantantion

--- src/interfaces/pokemons.ts
+++ src/interfaces/pokemons.ts
@@ -1,14 +1,23 @@
 export interface iPokemon {
   name: string;
-  rarity: string;
+  rarity: "C" | "R" | "E" | "L";
   pokedex: number;
   type_1: string;
-  type_2: string;
-  id: string | number;
+  type_2: string | null;
+  id: string;
 }
 
 export interface iPokemonUser extends iPokemon {
   user: string;
   price: number;
   on_marketplace: boolean;
+}
+
+export interface iPokemonUserUpdate {
+  rarity?: "C" | "R" | "E" | "L";
+  pokedex?: number;
+  type_1?: string;
+  type_2?: string | null;
+  price?: number;
+  on_marketplace?: boolean;
 }
